233a9032999e91c6aa941ae3e2ce58f708008cef
Buffahardt <kengelhardt@uni-osnabrueck.de>
2021-04-02 15:34:59 +0200

    final version

--- utils/img_ops.py
+++ utils/img_ops.py
@@ -2,11 +2,16 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import io
 
-def plot(image_batch, gen_images, gen_images_back, title):
-  #plots 3 images next to each other, original, generated and backwards generated
+def plot(image_batch, gen_images, gen_images_back):
+    '''plots 3 images next to each other.
+    Keyword arguments: original image,
+                       generated image,
+                       and backwards generated'''
+    # Define plot with one ow and three columns
     fig, ax = plt.subplots(1,3)
-    # convert image datatype, so that mathplotlib does not throw clipping error
-    #fig.subtitle(title, fontsize=9)
+    # Squeeze image will change the shape of the image from (1, hight, width, 3) to (hight, width, 3)
+    # Convert image datatype to unit8, else mathplotlib does throw clipping error.
+    # Multiply th eimage by *0.5+0.5 to undo the normalization (-1:1)
     ax[0].imshow(tf.squeeze(tf.image.convert_image_dtype(image_batch[0]*0.5 +0.5, dtype= tf.uint8)))
     ax[0].set_title("original image")
     ax[0].axis("off")
@@ -16,39 +21,50 @@ def plot(image_batch, gen_images, gen_images_back, title):
     ax[2].imshow(tf.squeeze(tf.image.convert_image_dtype(gen_images_back[0]*0.5 +0.5, dtype= tf.uint8)))
     ax[2].set_title("backwards generated")
     ax[2].axis("off")
+
     plt.show()
 
 def plot_image_cycle(generator_1, generator_2, dataset1, dataset2, ablation = False):
-  # for ablation studies we want to generate the same 30 test images for both classes
-  # thus we need to take 30 images from the dataset
-  # in the training process we only want to see one sample image cylcle from both classes
+  '''Creates a plot each dataset it shows a full image cycle - i.e. our network translates
+  an image from the domain of one domain X to domain Y and from domain Y back to X.
+  Keyword arguments:
+           dataset1, contains images of domain 1
+	   dataset_2,  contains images of domain 2
+           generator_1, translates images from domain 2 to domain 1 - i.e. generates domain 1 images
+  	   generator_2, translates images from domain 1 to domain 2 '''
+
+  # For ablation studies we want to generate the same 30 test images for both datasets
+  # So for ablation we set take_images to 30
   if ablation == True:
     take_images = 30
   else:
+    # Inside the training process we plot one sample image cylcle for both datasets
     take_images = 1
 
-  # image cycle dataset1 to 2 (and back)
-  for image_batch in dataset2.take(take_images):
-    gen_images = generator_1(image_batch)
-    gen_images_back = generator_2(gen_images)
-    #plots the images with description labels
-    plot(image_batch, gen_images, gen_images_back, title = "Generateor 1 cycle: ")
+  # Image generation cycle: from dataset 2 to dataset 1 and back
+  for image_batch_2 in dataset2.take(take_images):
+    # Feed original image into generator which translates it from domain 2 to domain 1
+    gen_images_1 = generator_1(image_batch)
+    # Feed generated image into generator which translates it from domain 1 to domain 2
+    gen_images_back_2 = generator_2(gen_images_1)
+    # Feed the resulting images into our plot function
+    plot(image_batch_2, gen_images_1, gen_images_back_2)
 
-  # image cycle dataset2 to 1 (and back)
+  # Repeat the same process for dataset 1
   for image_batch in dataset1.take(take_images):
     gen_images = generator_2(image_batch)
     gen_images_back = generator_1(gen_images)
-    plot(image_batch, gen_images, gen_images_back, title = "Generator 2 cycle: ")
+    plot(image_batch, gen_images, gen_images_back)
 
 
 def plot_to_tf_image(generator_1, generator_2, dataset_1, dataset_2):
-  """Creates a 2x2 grid plot and converts it to one singe PNG image.
-  Arguments: dataset1, contains images of domain 1
+  '''Creates a 2x2 grid plot and converts it to one single tf image.
+  Keyword arguments:
+             dataset1, contains images of domain 1
 	     dataset_2,  contains images of domain 2
-             generator_1, translates images from domain 2 to domain 1 
-	       - i.e. generates domain 1 images
+             generator_1, translates images from domain 2 to domain 1 - i.e. generates domain 1 images
   	     generator_2, translates images from domain 1 to domain 2 
-  Returns:   A png image converted to TF image"""  
+  Returns:   A png image converted to TF image'''  
 
   # Get one image for each datast
   for img_1, img_2 in tf.data.Dataset.zip((dataset_1, dataset_2)).take(1):
